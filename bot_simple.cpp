8b68051b28a18aae6886fd34f5c9d1c3f657f73e
Richard Willian R <98018644+richardwillramos@users.noreply.github.com>
2026-09-18 10:46:05 -0300

    18 /09 - v2.6

--- bot_simple.cpp
+++
@@ -1,9 +0,0 @@
-#include <Windows.h>
-
-static BOOL WINAPI DllMain(HMODULE hMod, DWORD reason, LPVOID) {
-    if (reason == DLL_PROCESS_ATTACH) {
-        DisableThreadLibraryCalls(hMod);
-        OutputDebugStringA("[BOT] DLL loaded OK\n");
-    }
-    return TRUE;
-}
